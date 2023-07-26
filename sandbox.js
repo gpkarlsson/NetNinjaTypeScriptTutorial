fd073e04acafc94ad1176f55cac0085b933c8770
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-07-26 12:37:14 -0500

    intro to types and type declaration

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+age = 40;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(40));
