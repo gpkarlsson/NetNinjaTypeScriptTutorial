bc527052d98933fcc3996598ca8f97622079198b
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-08-09 12:08:14 -0500

    working on classes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,23 @@
 "use strict";
-// const anchor = document.querySelector('a');
-// console.log(anchor?.href);
-// const form = document.querySelector('form')!;
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invOne);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 //inputs
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-// const anchor = document.querySelector('a');
+//classes
 
-// console.log(anchor?.href);
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// const form = document.querySelector('form')!;
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
 
+    format(){
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invOne);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invOne, invTwo)
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 console.log(form.children);
