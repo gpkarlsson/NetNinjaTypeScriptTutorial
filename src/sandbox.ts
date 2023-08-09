922e3571849a36e9a529b16eb8eea314861632fa
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-08-09 11:33:16 -0500

    type aliases lesson 9

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,21 +1,45 @@
-let greet: Function;
+// type StringOrNum = string | number;
 
-// greet = 'hello'
+// type objWithName = {
+//     name: string,
+//     uid: StringOrNum
+// }
 
-greet = () => {
-    console.log('hello, again')
+// const logDetails = (uid: StringOrNum, item: string) => {
+//     console.log(`${item} has a uid of ${uid}`);
+// }
+
+// const greet = (user: objWithName) => {
+//     console.log(`${user.name} says hello`);
+// }
+
+// const greetAgain = (user: objWithName) => {
+//     console.log(`${user.name} says hello`);
+// }
+//example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
 }
 
-const add = (a: number, b: number, c: number | string = 10) => {
-    //optional params always at the end
-    console.log(a + b);
-    console.log(c);
+//ex 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+};
+
+//ex 3
+
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
+console.log(`${ninja.name} is ${ninja.age} years old`);
 }
-
-add(5, 10);
-
-const minus = (a: number, b: number): number => {
-    return a + b;
-}
-
-let result = minus(10, 7);
