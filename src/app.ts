6f1dc0fe2115885c0f5eb9c39c46644400bf10bf
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-08-09 14:55:03 -0500

    working on rendering html templates

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
@@ -65,6 +66,11 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+//list template instance
+const ul = document.querySelector('ul')!;
+
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -75,6 +81,7 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end')
+
 });
 
